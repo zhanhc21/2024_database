47fc6e59fe08a274f4c73893895a9b3e8646054a
zhanhc <zhanhc21@mails.tsinghua.edu.cn>
2024-04-30 19:40:48 +0800

    lab4 limit

--- src/executors/limit_executor.cpp
+++ src/executors/limit_executor.cpp
@@ -2,16 +2,35 @@
 
 namespace huadb {
 
-LimitExecutor::LimitExecutor(ExecutorContext &context, std::shared_ptr<const LimitOperator> plan,
-                             std::shared_ptr<Executor> child)
-    : Executor(context, {std::move(child)}), plan_(std::move(plan)) {}
+    LimitExecutor::LimitExecutor(ExecutorContext &context, std::shared_ptr<const LimitOperator> plan,
+                                 std::shared_ptr<Executor> child)
+            : Executor(context, {std::move(child)}), plan_(std::move(plan)) {
+        offset_ = plan_->limit_offset_.value_or(0);
+        count_ = plan_->limit_count_.value_or(-1);
+    }
 
-void LimitExecutor::Init() { children_[0]->Init(); }
+    void LimitExecutor::Init() { children_[0]->Init(); }
 
-std::shared_ptr<Record> LimitExecutor::Next() {
-  // 通过 plan_ 获取 limit 语句中的 offset 和 limit 值
-  // LAB 4 BEGIN
-  return children_[0]->Next();
-}
+    std::shared_ptr<Record> LimitExecutor::Next() {
+        // 通过 plan_ 获取 limit 语句中的 offset 和 limit 值
+        // LAB 4 BEGIN
+        // 获取limit_count的引用
+        while (offset_ > 0) {
+            children_[0]->Next();
+            offset_--;
+        }
+
+        auto record = std::make_shared<Record>();
+
+        if (count_ > 0 && count_ != -1) {
+            record = children_[0]->Next();
+            count_--;
+        } else if (count_ == -1) {
+            record = children_[0]->Next();
+        } else {
+            return nullptr;
+        }
+        return record;
+    }
 
 }  // namespace huadb
--- src/executors/limit_executor.h
+++ src/executors/limit_executor.h
@@ -5,14 +5,19 @@
 
 namespace huadb {
 
-class LimitExecutor : public Executor {
- public:
-  LimitExecutor(ExecutorContext &context, std::shared_ptr<const LimitOperator> plan, std::shared_ptr<Executor> child);
-  void Init() override;
-  std::shared_ptr<Record> Next() override;
+    class LimitExecutor : public Executor {
+    public:
+        LimitExecutor(ExecutorContext &context, std::shared_ptr<const LimitOperator> plan,
+                      std::shared_ptr<Executor> child);
 
- private:
-  std::shared_ptr<const LimitOperator> plan_;
-};
+        void Init() override;
+
+        std::shared_ptr<Record> Next() override;
+
+    private:
+        std::shared_ptr<const LimitOperator> plan_;
+        uint32_t count_;
+        uint32_t offset_;
+    };
 
 }  // namespace huadb
