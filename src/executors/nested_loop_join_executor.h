15c43628085b7f65a5995d66295c2620547c3cff
zhanhc <zhanhc21@mails.tsinghua.edu.cn>
2024-05-12 18:33:20 +0800

    lab4 outer

--- src/executors/nested_loop_join_executor.h
+++ src/executors/nested_loop_join_executor.h
@@ -18,6 +18,15 @@ namespace huadb {
         std::shared_ptr<const NestedLoopJoinOperator> plan_;
         std::shared_ptr<Record> r_record_;
         std::shared_ptr<Record> s_record_;
+
+        size_t r_value_size_ = 0;
+        size_t s_value_size_ = 0;
+
+        std::vector<std::pair<int, bool>> r_table_;
+        std::vector<std::pair<int, bool>> s_table_;
+
+        int r_index_ = 0;
+        int s_index_ = 0;
     };
 
 }  // namespace huadb
